f61b823c20aaea5911ac982268070f0ee52dd448
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-06-09 18:30:59 +0200

    navbar apears in the page, needs some styling

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Products from "../../component/Products";
+import Header from "../../component/Header";
 
 const HomePage = () => {
   return (
 
     <div>
+      <Header />
         <h1>Home page</h1>
     <Products />
     </div>
---
+++ src/component/Logo/index.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+    return (
+      <>
+        <img  className="w-12 justify-start logo" src="/src/images/Eazy_logo.png" alt="logo" />
+      </>
+    );
+  }
+  
+  export default Logo;
